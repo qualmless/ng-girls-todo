c9f432a1c20116b23df482fc8b4f10ed521377f0
qualmless <terasmash@gmail.com>
2020-07-25 15:22:38 -0500

    new component: todo-item

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  template: `
+    {{item.title}}
+  `,
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() item;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
